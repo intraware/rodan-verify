ef6104193ce8df58f95d05c606a84420b211851a
celestix <i@anodite.me>
2025-07-30 03:02:05 +0530

    add base code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	// new => initiates a new verification session, takes email and userID as parameters
+	// returns a sessionID
+	mux.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {})
+	// verify => takes the sessionID and a verification code, verifies the code against the session
+	mux.HandleFunc("/verify", func(w http.ResponseWriter, r *http.Request) {})
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		panic(err)
+	}
+}
